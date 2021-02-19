7f43d01c21f018a8bd1c9b624a524b6241f4c0f4
Gustavo <gustavo@bigbangcomunicacao.com.br>
2021-02-19 18:00:08 -0300

    Add total_value() to Invoice

--- lib/entities/invoice.ex
+++ lib/entities/invoice.ex
@@ -1,3 +1,14 @@
 defmodule Entities.Invoice do
+  alias Entities.InvoiceLine, as: InvoiceLine
 
+  @spec total_value([%InvoiceLine{}]) :: integer()
+
+  def total_value(invoice_line_list) do
+    invoice_line_list
+    |> Enum.map(fn invoice_line ->
+      invoice_line
+      |> InvoiceLine.total_price()
+    end)
+    |> Enum.sum()
+  end
 end
