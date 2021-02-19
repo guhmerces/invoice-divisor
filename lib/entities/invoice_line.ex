626341f036ee34bd0c25ce2a1d5b1979689de09d
Gustavo <gustavo@bigbangcomunicacao.com.br>
2021-02-19 17:52:39 -0300

    Add total_price to InvoiceLine

--- lib/entities/invoice_line.ex
+++ lib/entities/invoice_line.ex
@@ -1,3 +1,11 @@
 defmodule Entities.InvoiceLine do
+  alias Entities.InvoiceLine, as: InvoiceLine
+
   defstruct name: nil, price: 0, qty: 0
+
+  @spec total_price(%InvoiceLine{}) :: integer()
+
+  def total_price(%InvoiceLine{} = invoice_line) do
+    invoice_line.price * invoice_line.qty
+  end
 end
