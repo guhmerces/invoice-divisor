d834e5cbfe55cf786588f181593a214f901f4541
Gustavo <gustavo@bigbangcomunicacao.com.br>
2021-02-19 17:34:41 -0300

    Create Mix project

---
+++ lib/invoice_divisor.ex
@@ -0,0 +1,3 @@
+defmodule InvoiceDivisor do
+
+end
---
+++ test/invoice_divisor_test.exs
@@ -0,0 +1,8 @@
+defmodule InvoiceDivisorTest do
+  use ExUnit.Case
+  doctest InvoiceDivisor
+
+  test "greets the world" do
+    assert InvoiceDivisor.hello() == :world
+  end
+end
